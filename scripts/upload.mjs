0e181eede63d2e4df6de5d241df6f185a8ab6b31
malautaro <malautaro@gmail.com>
2026-03-08 22:00:41 -0300

    updated upload.msj

--- scripts/upload.mjs
+++ scripts/upload.mjs
@@ -21,18 +21,18 @@ const supabase = createClient(supabaseUrl, supabaseServiceKey);
 const FILES_TO_UPLOAD = [
   // { file: 'table_regions.csv', table: 'regions', pk: 'reg_id' }, // no deberia updatearse a menos que haya una nueva region
   // { file: 'table_tournament.csv', table: 'tournament', pk: 'tour_id' }, //Update con torneos nuevos
-  //{ file: 'table_tournament_played.csv', table: 'tournament_played', pk: 'tour_id, teamA' }, // Verifica si tu PK es compuesta
-  // { file: 'table_maps_id.csv', table: 'maps_id', pk: 'map_id' },
-  // { file: 'table_maps_name_id.csv', table: 'maps_name_id', pk: 'map_id' },
-  // { file: 'table_match_id.csv', table: 'match_id', pk: 'series_id' },
+  // { file: 'table_tournament_played.csv', table: 'tournament_played', pk: 'tour_id, teamA' },   
+  // { file: 'table_players.csv', table: 'players', pk: 'player_id' }, //solo con players nuevos  
   // { file: 'table_teams.csv', table: 'teams', pk: 'team_id' },
-//{ file: 'table_players.csv', table: 'players', pk: 'player_id' }, //solo con players nuevos
 
+  { file: 'table_maps_name_id.csv', table: 'maps_name_id', pk: 'map_id' },
+  // { file: 'table_maps_id.csv', table: 'maps_id', pk: 'map_id' },
+  // { file: 'table_match_id.csv', table: 'match_id', pk: 'series_id' },
   // { file: 'table_draft.csv', table: 'draft', pk: 'series_id' },
-  { file: 'table_round_info.csv', table: 'round_info', pk: 'team_map_round_id' },
-  // { file: 'table_team_economy.csv', table: 'team_economy', pk: 'team_map_round_id' }, 
-  // { file: 'table_player_stats.csv', table: 'player_stats', pk: 'vlr_id-map, player' }, // puede estar mal 
-  // { file: 'table_player_performance.csv', table: 'player_performance', pk: 'vlr_id-map, player' },
+  // { file: 'table_round_info.csv', table: 'round_info', pk: 'team_map_round_id' },
+  { file: 'table_team_economy.csv', table: 'team_economy', pk: 'team_map_round_id' }, 
+  { file: 'table_player_stats.csv', table: 'player_stats', pk: 'vlr_id-map, player' }, // puede estar mal 
+  { file: 'table_player_performance.csv', table: 'player_performance', pk: 'vlr_id-map, player' },
 
 ];
 
